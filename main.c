e1efd6cc8be1a9a17a5cce2a9df11bbb900b90fc
mohamedabdelwahab2000 <eng.appo2@gmail.com>
2023-08-30 17:10:38 +0300

    main.c is modified

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+char name1[60] = "mohamed abdelwahab";
+const char name2[60] = "abo ali";
+
+
+int main()
+{
+    printf("dest = %s, src = %s\n", name1, name2);
+
+
+
+    printf("hello my bro\n");
+
+
+    return 0;
+}
